89f48fa69424b21e4a3e38b78992e9f3dfbdb04d
Majid Aram <majid.aram@arbetsformedlingen.se>
2016-04-29 16:30:44 +0200

    Now we can run custom tests for a system (dbSource)

--- common/src/main/groovy/db/CompareDbsBase/CompareCustomS2T_TestFactory.groovy
+++ common/src/main/groovy/db/CompareDbsBase/CompareCustomS2T_TestFactory.groovy
@@ -1,13 +1,14 @@
 package db.CompareDbsBase
 
 import org.testng.annotations.Factory
+import org.testng.annotations.Optional
 import org.testng.annotations.Parameters
 
 class CompareCustomS2T_TestFactory extends CompareS2T_TestFactoryBase {
 
-    @Parameters(["inputFile", "onlyEnabled"])
+    @Parameters(["inputFile", "onlyEnabled", "dbSource"])
     @Factory
-    public Object[] createCustomInstances(String inputFile, boolean onlyEnabled) {
-        return runCustom(inputFile, onlyEnabled)
+    public Object[] createCustomInstances(String inputFile, boolean onlyEnabled, @Optional ("") String dbSource) {
+        return runCustom(inputFile, onlyEnabled, dbSource)
     }
 }
