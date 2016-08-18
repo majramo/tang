0d4192cef4e3f691c7e3dcc8b2288c5c63643947
Majid Aram <majid.aram@arbetsformedlingen.se>
2016-08-18 10:08:37 +0200

    Check if table exists before running count Skip if not exists

--- common/src/main/groovy/db/CompareDbsBase/VerifyTruncatedTargetTable_Test.groovy
+++ common/src/main/groovy/db/CompareDbsBase/VerifyTruncatedTargetTable_Test.groovy
@@ -2,6 +2,8 @@ package db.CompareDbsBase
 
 import base.AnySqlCompareTest
 import org.testng.ITestContext
+import org.testng.Reporter
+import org.testng.SkipException
 import org.testng.annotations.Test
 
 import static dtos.base.Constants.dbRunTypeFirstRow
@@ -10,16 +12,19 @@ public class VerifyTruncatedTargetTable_Test extends AnySqlCompareTest{
     private static int row = 0
     private String targetDb;
     private String targetSql;
+    private String targetDbOwner;
     private String atgard;
     private final static String VERIFY_TRUNCATED_TABLE_QUERY = "SELECT COUNT(*) COUNT_ FROM %s"
-
+    def TARGET_TABLE_QUERY_ORACLE = "SELECT DISTINCT table_name FROM all_tab_cols WHERE table_name = '%s' AND NOT table_name IN (select view_name from all_views) AND OWNER = '%s'"
+    def TARGET_TABLE_QUERY_SQLSERVER = "SELECT DISTINCT Table_name FROM Information_schema.columns WHERE table_name = '%s'"
+    def table
 
     public VerifyTruncatedTargetTable_Test(targetDb, system, table, atgard) {
         super.setup()
         this.targetDb = targetDb
         this.atgard = atgard
-
-        String dbTargetOwner = settings."$targetDb".owner
+        this.table = table
+        targetDbOwner = settings."$targetDb".owner
         targetSql = "-- Verify truncated table size <$table> in system <$system> iz zero\n"
         targetSql += String.format(VERIFY_TRUNCATED_TABLE_QUERY, "$table")
 
@@ -28,17 +33,33 @@ public class VerifyTruncatedTargetTable_Test extends AnySqlCompareTest{
 
     @Test
     public void verifyTruncatedTargetTest(ITestContext testContext){
+        super.setTargetSqlHelper(testContext, targetDb)
+        reporterLogLn(reporterHelper.addIcons(getDbType(), getDbType(targetDb)))
         row++
         reporterLogLn("Row: <$row> TRUNCATE TABLE ");
         reporterLogLn("targetDb:  <$targetDb> ");
+        reporterLogLn("table: <$table> ");
         reporterLogLn("Atgard:    <$atgard> ");
         reporterLogLn("targetSql: <$targetSql> ");
         reporterLogLn("#########")
 
-        super.setTargetSqlHelper(testContext, targetDb)
-        reporterLogLn(reporterHelper.addIcons(getDbType(), getDbType(targetDb)))
+        //Search for table before executing, skip if table not exists
+        def targetTableSql = String.format(TARGET_TABLE_QUERY_ORACLE, table, targetDbOwner.toUpperCase())
+        if(getDbType(targetDb).equals("sqlserver")){
+            targetTableSql = String.format(TARGET_TABLE_QUERY_SQLSERVER, table)
+        }
+        def dbResult = getDbResult(targetDbSqlDriver, targetTableSql, dbRunTypeFirstRow)
+        if(dbResult == null){
+            Reporter.log("Table <$table> does not exist")
+            Reporter.log("targetTableSql <$targetTableSql>")
+            throw new SkipException("Table <$table> does not exist")
+        }
 
-        getDbResult(targetDbSqlDriver, targetSql, dbRunTypeFirstRow)
+        dbResult = getDbResult(targetDbSqlDriver, targetSql, dbRunTypeFirstRow)
+        def count = dbResult["COUNT_"]
+        reporterLogLn("Table count: <$count>")
+        tangAssert.assertEquals(count, 0, "Tabllen ska vara tom", "Tabellen har  <$count> rader, förväntat är <0>");
+
     }
 
 }
