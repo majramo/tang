c3cc0b2ea428430e8aa55e66409dc9da9bb5f59c
Majid Aram <majid.aram@arbetsformedlingen.se>
2016-10-14 16:28:37 +0200

    Typo

--- common/src/main/groovy/db/CompareDbsBase/VerifyTruncatedTargetTable_Test.groovy
+++ common/src/main/groovy/db/CompareDbsBase/VerifyTruncatedTargetTable_Test.groovy
@@ -58,7 +58,7 @@ public class VerifyTruncatedTargetTable_Test extends AnySqlCompareTest{
         dbResult = getDbResult(targetDbSqlDriver, targetSql, dbRunTypeFirstRow)
         def count = dbResult["COUNT_"]
         reporterLogLn("Table count: <$count>")
-        tangAssert.assertEquals(count, 0, "Tabllen ska vara tom", "Tabellen har  <$count> rader, förväntat är <0>");
+        tangAssert.assertEquals(count, 0, "Tabellen ska vara tom", "Tabellen har  <$count> rader, förväntat är <0>");
 
     }
 
