a0f94408fc5a05b32548a5356ab980a1c00ff2b4
aramd <196809>
2017-04-03 16:32:43 +0200

    Changing to target max and min diff comparasion

--- common/src/main/groovy/db/CompareDbsBase/CompareSizeCustomS2T_Test.groovy
+++ common/src/main/groovy/db/CompareDbsBase/CompareSizeCustomS2T_Test.groovy
@@ -75,8 +75,11 @@ public class CompareSizeCustomS2T_Test extends AnySqlCompareTest{
 
     private compareTableSizes(sourceDb, SqlHelper dbDriverSource, sourceDbResult, targetDb, dbDriverTarget, targetDbResult, String system, String inputFile) {
         int totalDiffCount = 0
-        float expectedDiff = 0
-        Map expectdValues = [:]
+        float expectedMaximumDiff = 0
+        float expectedMinimumDiff = 0
+        Map truncateTables = [:]
+        Map expectedMaximumDiffValues = [:]
+        Map expectedMinimumDiffValues = [:]
         float totalDiffCountExpected = 0
         def dbSourceResultCount = sourceDbResult.size
         def dbTargetResultCount = targetDbResult.size
@@ -96,22 +99,34 @@ public class CompareSizeCustomS2T_Test extends AnySqlCompareTest{
             excelObjectProvider.addColumnsToRetriveFromFile(["Table", "Action"])
             excelObjectProvider.addColumnsCapabilitiesToRetrieve("System", system)
             excelObjectProvider.addColumnsCapabilitiesToRetrieve("Action", "Truncate")
-            excelBodyRows = excelObjectProvider.getGdcObjects(10)
+            excelBodyRows = excelObjectProvider.getGdcRows()
             excelObjectProvider.printRow(excelBodyRows.unique(), ["System", "Table", "Truncate"])
             excelBodyRows.unique().each{
-                expectdValues[it["Table"] ] = 1
+               truncateTables[it["Table"] ] = "Truncate"
             }
 
             excelObjectProvider = new ExcelObjectProvider(inputFile)
-            excelObjectProvider.addColumnsToRetriveFromFile(["Table", "TargetSize"])
             excelObjectProvider.addColumnsCapabilitiesToRetrieve("System", system)
-            excelObjectProvider.addColumnsCapabilitiesToRetrieve("TargetSize", "-", DIFF)
-            excelBodyRows = excelObjectProvider.getGdcObjects(3)
-            excelObjectProvider.printRow(excelBodyRows.unique(), ["System", "Table", "TargetSize"])
+            excelObjectProvider.addColumnsToRetriveFromFile(["Table", "Action"])
+            excelObjectProvider.addColumnsCapabilitiesToRetrieve("TargetSizeMaximumDiff", "-", DIFF)
+            excelBodyRows = excelObjectProvider.getGdcRows()
+            excelObjectProvider.printRow(excelBodyRows.unique(), ["System", "Table", "TargetSizeMaximumDiff"])
 
             excelBodyRows.unique().each{
-                expectdValues[it["Table"] ] = it["TargetSize"]
+                expectedMaximumDiffValues[it["Table"] ] = it["TargetSizeMaximumDiff"]
             }
+
+            excelObjectProvider = new ExcelObjectProvider(inputFile)
+            excelObjectProvider.addColumnsCapabilitiesToRetrieve("System", system)
+            excelObjectProvider.addColumnsToRetriveFromFile(["Table", "Action"])
+            excelObjectProvider.addColumnsCapabilitiesToRetrieve("TargetSizeMinimumDiff", "-", DIFF)
+            excelBodyRows = excelObjectProvider.getGdcRows()
+            excelObjectProvider.printRow(excelBodyRows.unique(), ["System", "Table", "TargetSizeMinimumDiff"])
+
+            excelBodyRows.unique().each{
+                expectedMinimumDiffValues[it["Table"] ] = it["TargetSizeMinimumDiff"]
+            }
+
 
 
         }
@@ -124,7 +139,8 @@ public class CompareSizeCustomS2T_Test extends AnySqlCompareTest{
         uniqueDbResult.eachWithIndex { it, i ->
             def j = i + 1
             boolean  loopException = false
-            expectedDiff = 0
+            expectedMaximumDiff = 0
+            expectedMinimumDiff = 0
             def table = it[0]
             def row = "$j:$dbSourceResultCount $table"
             log.info("$row")
@@ -154,42 +170,84 @@ public class CompareSizeCustomS2T_Test extends AnySqlCompareTest{
                 str = aggregate(str, "Exception i target <$targetDb> $e")
             }
             str = aggregate(str, "target size <$targetSize>")
+            String truncateTable = truncateTables[table]
+            String expectedMaximumDiffTableValue = expectedMaximumDiffValues[table]
+            String expectedMinimumDiffTableValue = expectedMinimumDiffValues[table]
+            if(truncateTable != null) {
+                str = aggregate(str, "Truncate")
+            }
+            if(expectedMinimumDiffTableValue != "" && expectedMinimumDiffTableValue != null) {
+                expectedMinimumDiff = (Float.parseFloat(expectedMinimumDiffTableValue) * 100).trunc(2)
+                str = aggregate(str, "Expected minimum diff value <$expectedMinimumDiff %>")
+            }
+            if(expectedMaximumDiffTableValue != "" && expectedMaximumDiffTableValue != null) {
+                expectedMaximumDiff = (Float.parseFloat(expectedMaximumDiffTableValue) * 100).trunc(2)
+                str = aggregate(str, "Expected maximum diff value <$expectedMaximumDiff %>")
+            }
 
-            diffCount = (sourceSize - targetSize).abs()
-            def SourceTargetMax = sourceSize
-            if(targetSize > sourceSize){
-                SourceTargetMax = targetSize
-            }
-            float diffCountPercent = 0
-            if(SourceTargetMax > 0){
-                diffCountPercent =  (100 * diffCount / SourceTargetMax).abs()
-            }
-            reporterLogLn(row.padRight(25) + " <D " + "$diffCount>".padLeft(12) + " <S " + "$sourceSize>".padLeft(12)  + " <T " + "$targetSize>".padLeft(12))
+            //Comparing
+            if(truncateTables[table]){
+                if (targetSize > 0 || loopException) {
+                    numberOfTableDiff++
+                    nok = aggregate(nok, "$str a. Table $table has <$targetSize> rows, expected to be truncated\n\n")
+                } else {
+                    ok = aggregate(ok, "$str a. Table $table has <$targetSize> rows as expected, is truncated\n\n")
+                }
 
-            totalDiffCount += diffCount
-            String expectedTableValue = expectdValues[table]
-            if(expectedTableValue != "" && expectedTableValue != null ){
-                expectedDiff = (Float.parseFloat(expectedTableValue)*100).trunc(2)
-                totalDiffCountExpected += (expectedDiff * SourceTargetMax / 100).round(0)
-            }
-            if(expectedDiff > 0) {
-                //Todo: Att hantera vid generering då vi kan utöka och få en diff >100%
-                if(sourceSize < targetSize || loopException){
-                    nok = aggregate(nok, "$str Table $table source <$sourceSize> has less rows than target <$targetSize>\n\n")
-                }else {
-                    if (diffCountPercent != expectedDiff || loopException) {
+            }else {
+                diffCount = (sourceSize - targetSize).abs()
+                def SourceTargetMax = sourceSize
+                if (targetSize > sourceSize) {
+                    SourceTargetMax = targetSize
+                }
+                float diffCountPercent = 0
+                if (SourceTargetMax > 0) {
+                    diffCountPercent = (100 * diffCount / SourceTargetMax).abs()
+                }
+                reporterLogLn(row.padRight(25) + " <D " + "$diffCount>".padLeft(12) + " <S " + "$sourceSize>".padLeft(12) + " <T " + "$targetSize>".padLeft(12))
+
+                totalDiffCount += diffCount
+
+
+                if(SourceTargetMax > 0) {
+                    totalDiffCountExpected += (expectedMaximumDiff * SourceTargetMax / 100).round(0)
+                }
+                if (expectedMaximumDiff > 0) {
+                    //Todo: Att hantera vid generering då vi kan utöka och få en diff >100%
+                    if (diffCountPercent > expectedMaximumDiff || loopException) {
                         numberOfTableDiff++
-                        nok = aggregate(nok, "$str Table $table has <$diffCount> diff, <% $diffCountPercent>, expected in target was <% $expectedDiff>\n\n")
+                        nok = aggregate(nok, "$str Table $table has <$diffCount> diff, <$diffCountPercent %>, expected maximum diff in target was <$expectedMaximumDiff %>\n\n")
+                    } else {
+                        ok = aggregate(ok, "$str Table $table has <$diffCount> diff, <$diffCountPercent %>, expected maximum diff in target was <$expectedMaximumDiff %>\n\n")
+                    }
+                } else {
+                    if (diffCountPercent > 0 || loopException) {
+                        numberOfTableDiff++
+                        nok = aggregate(nok, "$str table $table has <$diffCount> diff, <$diffCountPercent>, expected maximum diff in target was <0.0 %>\n\n")
+                    } else {
+                        ok = aggregate(ok, str)
                     }
                 }
-            }else{
-                if (diffCountPercent > 0 || loopException) {
-                    numberOfTableDiff++
-                    nok = aggregate(nok, "$str table $table has <$diffCount> diff, <% $diffCountPercent>, expected in target was <% 0.0>\n\n")
-                }else{
-                    ok = aggregate(ok, str)
+                if (expectedMinimumDiff > 0) {
+                    //Todo: Att hantera vid generering då vi kan utöka och få en diff >100%
+                    if (diffCountPercent < expectedMinimumDiff || loopException) {
+                        numberOfTableDiff++
+                        nok = aggregate(nok, "$str a. Table $table has <$diffCount> diff, <$diffCountPercent %>, expected minimum diff in target was <$expectedMinimumDiff %>\n\n")
+                    } else {
+                        ok = aggregate(ok, "$str b. Table $table has <$diffCount> diff, <$diffCountPercent %>, expected minimum diff in target was <$expectedMinimumDiff %>\n\n")
+                    }
+
+                } else {
+                    if (diffCountPercent < 0 || loopException) {
+                        numberOfTableDiff++
+                        nok = aggregate(nok, "$str c. table $table has <$diffCount> diff, <$diffCountPercent %>, expected minimum diff in target was <0.0 %>\n\n")
+                    } else {
+                        ok = aggregate(ok, str)
+                    }
                 }
             }
+            //Comparing
+
             numberOfTablesChecked++
 
         }
