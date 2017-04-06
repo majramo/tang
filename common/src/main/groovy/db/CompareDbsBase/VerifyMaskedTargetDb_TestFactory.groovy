7ec7f45e49f906cb7892aa6f1622e1de25c9d786
aramd <196809>
2017-04-06 12:41:23 +0200

    Input files is dynamic (one for each system)

--- common/src/main/groovy/db/CompareDbsBase/VerifyMaskedTargetDb_TestFactory.groovy
+++ common/src/main/groovy/db/CompareDbsBase/VerifyMaskedTargetDb_TestFactory.groovy
@@ -1,5 +1,6 @@
 package db.CompareDbsBase
 
+import dtos.SettingsHelper
 import excel.ExcelObjectProvider
 import org.testng.ITestContext
 import org.testng.Reporter
@@ -8,27 +9,28 @@ import org.testng.annotations.Parameters
 
 class VerifyMaskedTargetDb_TestFactory {
 
+    SettingsHelper settingsHelper = SettingsHelper.getInstance()
+    def settings = settingsHelper.settings
 
-
-    @Parameters(["inputFileColumn", "systemColumn"] )
+    @Parameters(["systemColumn"] )
     @Factory
-    public Object[] createVerifyTruncatedInstances(ITestContext testContext, String inputFileColumn, String systemColumn) {
+    public Object[] createVerifyTruncatedInstances(ITestContext testContext, String systemColumn) {
 
         def targetDb = systemColumn.toLowerCase() + "_Target"
         def sourceDb = systemColumn.toLowerCase() + "_Source"
-//        targetDb = systemColumn.toLowerCase() + "_Source"
-//        sourceDb = systemColumn.toLowerCase() + "_Target"
         def system = systemColumn[0].toUpperCase() + systemColumn[1..-1].toLowerCase()
         def result = [];
 
-        ExcelObjectProvider excelObjectProvider = new ExcelObjectProvider(inputFileColumn)
+        def systemInputFile = settings.systemInputFile + systemColumn.toLowerCase() + ".xls"
+
+        ExcelObjectProvider excelObjectProvider = new ExcelObjectProvider(systemInputFile)
         excelObjectProvider.addColumnsToRetriveFromFile(["Table", "Column", "Action", "Masking"])
         excelObjectProvider.addColumnsCapabilitiesToRetrieve("System", system)
-        excelObjectProvider.addColumnsCapabilitiesToRetrieve("Action", "Avidentifiera")
+        excelObjectProvider.addColumnsCapabilitiesToRetrieve("Action", "Mask")
         def excelBodyRows = excelObjectProvider.getGdcRows()
         excelObjectProvider.printRow(excelBodyRows, ["System", "Table", "Column", "Action"])
 
-        Reporter.log("Number of lines read <$excelBodyRows.size>")
+        Reporter.log("Lines read <$excelBodyRows.size>")
         Reporter.log("Action <Masking> ")
         excelBodyRows.unique().eachWithIndex { excelRow, index ->
             def table = excelRow["Table"]
