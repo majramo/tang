811d345414563eed74fd8ffa7e8a80f91f76292e
Majid Aram <majid.aram@arbetsformedlingen.se>
2017-11-08 13:14:39 +0100

    Möjlighet att styra med enabled värde i xml

--- common/src/main/groovy/db/CompareDbsBase/CompareCommonS2T_TestFactory.groovy
+++ common/src/main/groovy/db/CompareDbsBase/CompareCommonS2T_TestFactory.groovy
@@ -6,10 +6,10 @@ import org.testng.annotations.Parameters
 
 class  CompareCommonS2T_TestFactory extends CompareS2T_TestFactoryBase{
 
-    @Parameters(["inputFileColumn", "systemColumn", "considerSystemTableColumnAnalyse", "actionTypeColumn"] )
+    @Parameters(["inputFileColumn", "systemColumn", "considerSystemTableColumnAnalyse", "actionTypeColumn", "enabledColumn"] )
     @Factory
-    public Object[] createCommonInstances(String inputFileColumn, String systemColumn, @Optional ("") String considerSystemTableColumnAnalyse, @Optional ("") String actionTypeColumn) {
+    public Object[] createCommonInstances(String inputFileColumn, String systemColumn, @Optional ("") String considerSystemTableColumnAnalyse, @Optional ("") String actionTypeColumn, @Optional ("") String enabledColumn) {
 
-        return runCommon(inputFileColumn, systemColumn, considerSystemTableColumnAnalyse, actionTypeColumn)
+        return runCommon(inputFileColumn, systemColumn, considerSystemTableColumnAnalyse, actionTypeColumn, enabledColumn)
     }
 }
