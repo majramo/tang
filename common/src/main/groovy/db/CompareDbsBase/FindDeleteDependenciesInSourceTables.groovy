36df93211769a7d3cf6c8652ef26ee2ffe310e5e
Majid Aram <majid.aram@arbetsformedlingen.se>
2018-06-28 11:26:30 +0200

    This class uses relationsships between tables to find out in which order the Delete of rows in each table (not Delete Rule = cascade)  should be executed in proper way to maintain integrity in DB (Oracle)

--- common/src/main/groovy/db/CompareDbsBase/FindDeleteDependenciesInSourceTables.groovy
+++ common/src/main/groovy/db/CompareDbsBase/FindDeleteDependenciesInSourceTables.groovy
@@ -10,14 +10,21 @@ import org.testng.annotations.Test
 
 import static dtos.base.Constants.dbRunTypeRows
 
+/*
+ This class uses relationsships between tables to find out in which order the Delete of rows in each table (not Delete Rule = cascade)
+ should be executed in proper way to maintain integrity in DB (Oracle)
+Inputs are:
+the source system
+the table to delete records in
+the where statement used when deleteing
+the "delete statments" will be in the report
+ */
+
 public class FindDeleteDependenciesInSourceTables extends AnySqlCompareTest{
     private final static Logger log = Logger.getLogger("FDDS ")
     def actionTables = [:]
-    def actionTablesWhere = [:]
     def childTables = [:]
     def deleteRelations = [:]
-    def constraints = [:]
-    def deleteStr = ""
     private String SOURCE_TABLE_QUERY_ORACLE_FIND = """
 SELECT CHILD_TABLE, CHILDCOL, position, PARENT_TABLE, PARENTCOL, delete_rule, bt,  
 'SELECT ' || CHILDCOL || ' FROM ' || CHILD_TABLE || ' WHERE ' || CHILDCOL || ' IN ( ' || 'SELECT ' ||PARENTCOL || ' FROM ' ||PARENT_TABLE || ' WHERE %s )' WHERESTR ,
@@ -50,12 +57,12 @@ order by 7,6,4,2
     def counter = 1
     def indentNo = 0
     def indentString = ""
-    def parentTable
+    def startTable
     def source_R_Relations
 
-    @Parameters(["systemColumn", "tableColumn", "whereParentDelete"] )
+    @Parameters(["systemColumn", "startTableColumn", "deleteStatementColumn"] )
     @Test
-    public void findDependenciesInSourceTables_test(String systemColumn, String tableColumn, String whereParentDelete, ITestContext testContext) {
+    public void findDependenciesInSourceTables_test(String systemColumn, String startTableColumn, String deleteStatementColumn, ITestContext testContext) {
         super.setup()
         def (ExcelObjectProvider excelObjectProvider, String system, Object targetDb, Object sourceDb) = SystemPropertiesInitation.getSystemData(systemColumn)
         sourceDb = 'atg_Target'
@@ -64,23 +71,22 @@ order by 7,6,4,2
         super.setSourceSqlHelper(testContext, sourceDb)
         source_R_Relations = sourceDbSqlDriver.sqlConRun("Get data ", dbRunTypeRows, SOURCE_TABLE_QUERY_ORACLE_FIND, 0, sourceDb)
 
-        parentTable = tableColumn.trim().toUpperCase()
-        def doneTable = []
+        startTable = startTableColumn.trim().toUpperCase()
         indentString = ""
         indentNo = 0
         reporterLogLn("");
-        reporterLogLn("# " + "$counter".padLeft(4) + ": Parent: <$parentTable>");
+        reporterLogLn("# " + "$counter".padLeft(4) + ": Parent: <$startTable>");
 
-        actionTables[parentTable] = parentTable
-        printRelations(parentTable)
+        actionTables[startTable] = startTable
+        printRelations(startTable)
 
         reporterLogLn("\n\n--Delete");
-        findDeleteRelations(parentTable, whereParentDelete)
-        deleteRelations["$parentTable"] = "DELETE $parentTable $whereParentDelete;"
+        findDeleteRelations(startTable, deleteStatementColumn)
+        deleteRelations["$startTable"] = "DELETE $startTable $deleteStatementColumn;"
 
         def deleteRelationsCount = deleteRelations.size()
         deleteRelations.eachWithIndex { k, v, i->
-            reporterLogLn("--${i + 1}:$deleteRelationsCount  $k\n$v")
+            reporterLogLn("--${i + 1}:$deleteRelationsCount  ($startTable-->) $k\n$v")
         }
     }
 
@@ -91,7 +97,7 @@ order by 7,6,4,2
         indentNo++
         source_R_Relations.findAll {it["PARENT_TABLE"]== "$parent"}.each { child->
             def childTable = child["CHILD_TABLE"].toString().toUpperCase()
-            if (!this.parentTable.contains(childTable) && childTable != parent)  {
+            if (!startTable.contains(childTable) && childTable != parent)  {
                 def deleteRule = child["DELETE_RULE"]
                 def indentNoStr = "$indentNo "
                 if(indentNo.equals(1)){
@@ -113,9 +119,6 @@ order by 7,6,4,2
 
     private String findDeleteRelations(String parent, whereParentDelete) {
         def parentTable = parent.toUpperCase()
-        indentString += "-"
-
-        indentNo++
         source_R_Relations.findAll {it["PARENT_TABLE"]== "$parent"}.each { actionTable->
             def whereChildtDelete = ""
             def childTable = actionTable["CHILD_TABLE"].toString().toUpperCase()
@@ -129,11 +132,8 @@ order by 7,6,4,2
 
             findDeleteRelations(childTable, whereChildtDelete)
             if (deleteRule != "CASCADE") {
-                deleteRelations["$parentTable $childTable"] = "DELETE $childTable $whereChildtDelete;\n-- SELECT * FROM $childTable $whereChildtDelete;\n\n"
+                deleteRelations["$parentTable --> $childTable"] = "DELETE $childTable $whereChildtDelete;\n-- SELECT * FROM $childTable $whereChildtDelete;\n\n"
             }
         }
     }
-
-
-
 }
