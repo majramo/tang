062c32bd0e12863c60b99f94d10745a902702a33
majramo@gmail.com <Cuinhel2021>
2019-01-09 14:36:30 +0100

    If dbPassword is a formula like "B1&G1" then we put owner and pwd togheter to make dbPassword

--- common/src/main/groovy/base/InitDbSettings.groovy
+++ common/src/main/groovy/base/InitDbSettings.groovy
@@ -2,6 +2,9 @@ package base
 
 import dtos.SettingsHelper
 import excel.ExcelObjectProvider
+import org.apache.poi.hssf.usermodel.HSSFWorkbook
+import org.apache.poi.ss.usermodel.FormulaEvaluator
+import org.apache.poi.ss.usermodel.Workbook
 
 /**
  * Created by majaraaa on 2016-06-09.
@@ -13,7 +16,7 @@ class InitDbSettings {
         def settings = settingsHelper.settings
 
         ExcelObjectProvider excelObjectProvider = new ExcelObjectProvider("/configFiles/databases.xls")
-        excelObjectProvider.addColumnsToRetriveFromFile(["dbName", "owner", "dbDriverName", "dbDriver", "dbUrl", "dbUserName", "dbPassword", "dbTestDataBase"])
+        excelObjectProvider.addColumnsToRetriveFromFile(["dbName", "owner", "dbDriverName", "dbDriver", "dbUrl", "dbUserName", "pwd", "dbPassword", "dbTestDataBase"])
         ArrayList<Object[][]> databases = excelObjectProvider.getGdcRows()
 
         databases.each {
@@ -26,7 +29,13 @@ class InitDbSettings {
                 dbSettings['dbDriver'] = (it["dbDriver"]).toString().trim()
                 dbSettings['dbUrl'] = (it["dbUrl"]).toString().trim()
                 dbSettings['dbUserName'] = (it["dbUserName"]).toString().trim()
-                dbSettings['dbPassword'] = (it["dbPassword"]).toString().trim()
+                def dbPassword = (it["dbPassword"]).toString().trim()
+                if (dbPassword.matches("B.*&G.*")){
+                    //If dbPassword is a formula  like above then we put owner and pwd togheter
+                    def pwd = (it["pwd"]).toString().trim().replaceAll(/\..*/, "")
+                    dbPassword =  dbSettings['owner'] + pwd
+                }
+                dbSettings['dbPassword'] = dbPassword
                 dbSettings['dbTestDataBase'] = (it["dbTestDataBase"]).toString().trim()
                 settings."${dbName}" = dbSettings
             }
